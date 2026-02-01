6ee4b800be73f3809e994309de90b4223f6dc6a7
Monty <montather.al-hassy@mohawkcollege.ca>
2026-01-31 20:36:58 -0500

    Add
    -enemy and support energy vaules
    -if statement and resulting output

--- Al-Hassy-Montather-a1-TerminalProject/Program.cs
+++ Al-Hassy-Montather-a1-TerminalProject/Program.cs
@@ -1,17 +1,33 @@
 ﻿    // Intro plus naming
 Console.WriteLine("A stong brave warrior has been engaged in a fight with the villan " +
-    "Ryoza to the death with the planet on the line. " +
-    "What is the name of this couragous fighter?");
+    "Ryoza to the death with the planet on the line.\n" +
+    "What is the name of this courageous fighter?");
     // Fighter name stored
 string fighterName = Console.ReadLine();
+Console.WriteLine();
 
-    // Follow up plus energy prompt
+// Follow up plus energy prompt
 Console.WriteLine($"{fighterName} has a limited amount of energy left to beat Ryoza. " +
-    $"How much power left do you belive you have to fight off Ryoza?");
+    $"How much power left do you believe they have to fight off Ryoza?");
 
     // Store energy amount
 int remainingEnergy = int.Parse(Console.ReadLine());
+Console.WriteLine();
 
     // Follow up plus prompt about if support was chosen
-Console.WriteLine($"Before heading off for the fight of thier life, {fighterName} had the choice to bring " +
-    $"support from the Druin fighters. Did {fighterName} bring them along?");
+Console.WriteLine($"Before heading off for the fight of thier life, {fighterName} had the choice to bring\n" +
+    $"support from the Drazko fighters or go at it alone, thinking it'd be less restrictive.\nDid {fighterName} bring them along?");
+    
+    // Support respoinse vaule saved
+string drazkoSupport = Console.ReadLine();
+Console.WriteLine();
+
+    // Enemy  + suppport energy establised 
+int ryozaEnergy = 1000;
+int drazkoSupportEnergy = 500;
+
+if ( drazkoSupport == "Yes" || drazkoSupport == "yes")
+{
+    if (remainingEnergy + drazkoSupportEnergy > ryozaEnergy)
+        Console.WriteLine("Well done! You and your team manage to beat the fierce Ryoza!");
+}
