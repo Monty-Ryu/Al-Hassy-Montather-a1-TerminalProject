04fa98698987fc2e93a0099032609b4a9cda3ff7
Monty <montather.al-hassy@mohawkcollege.ca>
2026-02-01 21:52:45 -0500

    Add
    -else statement informing player of loss

--- Al-Hassy-Montather-a1-TerminalProject/Program.cs
+++ Al-Hassy-Montather-a1-TerminalProject/Program.cs
@@ -1,56 +1,58 @@
-﻿    // Intro plus naming
-using System;
-using System.ComponentModel.Design;
-
-Console.WriteLine("A stong brave warrior has been engaged in a fight with the villan " +
-    "Ryoza to the death with the planet on the line.\n" +
+﻿// Intro plus naming
+// Fighter name stored
+Console.WriteLine("A stong brave warrior has been engaged in a fight with the villan Ryoza to the death with the planet on the line.\n" +
     "What is the name of this courageous fighter?");
-    // Fighter name stored
+
+Console.WriteLine();
 string fighterName = Console.ReadLine();
 Console.WriteLine();
 
-    // Follow up plus energy prompt
+// Follow up plus energy prompt
+// Store energy amount
 Console.WriteLine($"{fighterName} has a limited amount of energy left to beat Ryoza.\n" +
     $"How much power left do you believe they have to fight off Ryoza?");
 
-    // Store energy amount
+Console.WriteLine();
 int remainingEnergy = int.Parse(Console.ReadLine());
 Console.WriteLine();
 
-    // Follow up plus prompt about if support was chosen
-Console.WriteLine($"Before heading off for the fight of thier life, {fighterName} had the choice to bring\n" +
-    $"support from the Drazko fighters or go at it alone, thinking it'd be less restrictive.\nDid {fighterName} bring them along?");
+// Follow up plus prompt about if support was chosen
+// Support response vaule saved
+Console.WriteLine($"Before heading off for the fight of thier life, " +
+    $"{fighterName} had the choice to bring support from the Drazko fighters or go at it alone, thinking it'd be less restrictive. " +
+    $"Did {fighterName} bring them along?");
     
-    // Support respoinse vaule saved
+Console.WriteLine();
 string drazkoSupport = Console.ReadLine();
 Console.WriteLine();
 
-    // Enemy  + suppport energy establised 
+// Enemy + suppport energy establised 
+// Check if response is true or not
 int ryozaEnergy = 1000;
 int drazkoSupportEnergy = 500;
 
-    // Check if response is true or not
 if (drazkoSupport == "Yes" || drazkoSupport == "yes")
 {
+    // Enough total energy to pass  check. Inform of win
     if (remainingEnergy + drazkoSupportEnergy > ryozaEnergy)
     {
-        // Enough total energy, inform of win
-        Console.WriteLine("Well done! You and your team manage to beat the fierce Ryoza!");
+        Console.WriteLine($"Well done! {fighterName} and the team have manage to beat the fierce Ryoza!");
     }
     else
     {
-        // Not enough combined energy, inform of loss
-        Console.WriteLine($" Oh nooooooooooooo!!! {fighterName} and team Drazko did not have enough energy to beat Ryoza.");
+        // Not enough total energy to pass check. Inform of loss 
+        Console.WriteLine($"Oh nooooooooooooo!!! {fighterName} and team Drazko did not have enough energy to beat Ryoza.");
     }
 }
-
-else if (remainingEnergy >  ryozaEnergy)
+        // Check if player energy is greater than enemy. Inform of win
+else if (remainingEnergy > ryozaEnergy)
 {
-        Console.WriteLine($"Well done! {fighterName}s resolve and energy was enough to defeat the villanous Ryoza!!");
+    Console.WriteLine($"Well done! {fighterName}s resolve and remaining energy was enough to defeat the villanous Ryoza!!");
         
 }
 
+        // No correct response given or other checks fail. Inform player of loss
 else
 {
-    
+    Console.WriteLine($"Oh nooooooooo!!! {fighterName}s remaining energy was overestimated and has lost to the villanous Ryoza!!");
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Al-Hassy-Montather-a1-TerminalProject
+
+
+
 Simple text adventure.
+
+
+
+How to play: 
+
+Go through questions and give answers. Give a numeric value of up to 4 digits when prompted about energy. 
+
